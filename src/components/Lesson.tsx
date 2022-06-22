5b71a487ee40916462da42a90e269f26c35ac830
jey <andre.marqquez@gmail.com>
2022-06-22 00:47:47 -0300

    feat: adiciona props da GraphQL, componente Lessons Dinamico

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,9 +1,43 @@
 import { Link } from 'react-router-dom';
+import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-function Lesson() {
+interface LessonsProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+function Lesson(props: LessonsProps) {
+  const isLessonsAvailableAt = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de ' MMMM' • 'k'h'mm ", {
+    locale: ptBR,
+  });
+
   return (
     <Link to="#">
-      <span>Domingo • 20 de junho • 19h00</span>
+      <span className="text-gray-300">{availableDateFormatted}</span>
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          {isLessonsAvailableAt ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+          <span className="text-xs rounded px-2 py-[0.125rem] border border-green-300 font-bold">
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+      </div>
     </Link>
   );
 }
