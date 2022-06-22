f82244b3588116c99f8dee8785d499882db7d137
jey <andre.marqquez@gmail.com>
2022-06-21 23:52:34 -0300

    feat: router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Event from './pages/Event';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Event />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,11 @@
-
+import { Link } from 'react-router-dom';
 
 function Lesson() {
-  return <div>Lesson</div>;
+  return (
+    <Link to="#">
+      <span>Domingo • 20 de junho • 19h00</span>
+    </Link>
+  );
 }
 
 export default Lesson;
